f441da67cf7d7e2f0ec63ae974c3841a9fd86020
Zehs <andrey5764zehsteam@gmail.com>
2024-02-19 21:53:32 -0600

    Attempt to fix EnemyAIPatch

--- Hitmarker/Patches/EnemyAIPatch.cs
+++ Hitmarker/Patches/EnemyAIPatch.cs
@@ -19,11 +19,13 @@ internal class EnemyAIPatch
     [HarmonyPrefix]
     static void HitEnemyOnLocalClientPatch(ref EnemyAI __instance, PlayerControllerB playerWhoHit, int force)
     {
+        if (playerWhoHit == null) return;
+
         bool fromLocalPlayer = StartOfRound.Instance.localPlayerController == playerWhoHit;
 
         if (fromLocalPlayer)
         {
-            HitmarkerBase.mls.LogInfo($"[LOCAL] HitEnemyOnLocalClient();\n{__instance.enemyType.enemyName} ({__instance.enemyHP - force} HP)");
+            HitmarkerBase.mls.LogInfo($"[LOCAL] HitEnemyOnLocalClient(); {playerWhoHit.playerUsername} hit {__instance.enemyType.enemyName} ({__instance.enemyHP - force} HP)");
 
             HitEnemy(__instance, playerWhoHit, force, true);
         }
@@ -33,12 +35,14 @@ internal class EnemyAIPatch
     [HarmonyPrefix]
     static void HitEnemyServerRpcPatch(ref EnemyAI __instance, int playerWhoHit, int force)
     {
+        if (playerWhoHit == -1) return;
+
         PlayerControllerB playerWhoHitScript = StartOfRound.Instance.allPlayerScripts[playerWhoHit];
         bool fromLocalPlayer = StartOfRound.Instance.localPlayerController == playerWhoHitScript;
 
         if (!fromLocalPlayer)
         {
-            HitmarkerBase.mls.LogInfo($"[SERVER] HitEnemyServerRpc();\n{__instance.enemyType.enemyName} ({__instance.enemyHP - force} HP)");
+            HitmarkerBase.mls.LogInfo($"[SERVER] HitEnemyServerRpc(); {playerWhoHitScript.playerUsername} hit {__instance.enemyType.enemyName} ({__instance.enemyHP - force} HP)");
 
             HitEnemy(__instance, playerWhoHitScript, force, false);
         }
@@ -48,6 +52,8 @@ internal class EnemyAIPatch
     [HarmonyPrefix]
     static void HitEnemyClientRpcPatch(ref EnemyAI __instance, int playerWhoHit, int force)
     {
+        if (playerWhoHit == -1) return;
+
         bool isHostOrServer = NetworkManager.Singleton.IsHost || NetworkManager.Singleton.IsServer;
 
         PlayerControllerB playerWhoHitScript = StartOfRound.Instance.allPlayerScripts[playerWhoHit];
@@ -55,7 +61,7 @@ internal class EnemyAIPatch
 
         if (!isHostOrServer && !fromLocalPlayer)
         {
-            HitmarkerBase.mls.LogInfo($"[CLIENT] HitEnemyClientRpc();\n{__instance.enemyType.enemyName} ({__instance.enemyHP - force} HP)");
+            HitmarkerBase.mls.LogInfo($"[CLIENT] HitEnemyClientRpc(); {playerWhoHitScript.playerUsername} hit {__instance.enemyType.enemyName} ({__instance.enemyHP - force} HP)");
 
             HitEnemy(__instance, playerWhoHitScript, force, false);
         }
@@ -63,7 +69,7 @@ internal class EnemyAIPatch
 
     private static void HitEnemy(EnemyAI enemyAI, PlayerControllerB playerWhoHit, int damage, bool fromLocalPlayer)
     {
-        if (playerWhoHit == null) return;
+        if (enemyAI == null || playerWhoHit == null) return;
         if (enemyAI.enemyHP <= 0) return;
         if (!enemyAI.enemyType.canDie) return;
 
@@ -72,6 +78,8 @@ internal class EnemyAIPatch
 
         string enemyName = GetEnemyName(enemyAI);
         bool killedEnemy = enemyAI.enemyHP - damage <= 0;
+
+        LogInfo(enemyAI, playerWhoHit, damage, fromLocalPlayer);
 
         if (fromLocalPlayer)
         {
@@ -86,6 +94,18 @@ internal class EnemyAIPatch
             string message = fromLocalPlayer ? $"Killed {enemyName}" : $"{playerWhoHit.playerUsername} Killed {enemyName}";
             CanvasBehaviour.Instance.ShowKillMessage(message.Trim(), fromLocalPlayer);
         }
+    }
+
+    private static void LogInfo(EnemyAI enemyAI, PlayerControllerB playerWhoHit, int damage, bool fromLocalPlayer)
+    {
+        string info = string.Empty;
+
+        info += $"HitEnemy - {enemyAI.enemyType.enemyName}\n";
+        info += $"PlayerWhoHit - {playerWhoHit.playerUsername}\n\n";
+        info += $"Hit from LocalPlayer? {fromLocalPlayer}\n";
+        info += $"Current HP: {enemyAI.enemyHP}, Damage: {damage}, New HP: {enemyAI.enemyHP - damage}\n";
+
+        HitmarkerBase.mls.LogInfo($"\n\n{info.Trim()}\n");
     }
 
     private static string GetEnemyName(EnemyAI enemyAI)
